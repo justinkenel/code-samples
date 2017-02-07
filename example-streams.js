631e5a566375b84c8d664e57b892082e0cb9fd1b
Justin Kenel <jfkenel@gmail.com>
2017-02-07 10:20:50 -0500

    Added example using event-stream and through2
    The example (example-stream.js) uses event-stream, through2 and fs
    to process lines of text from a file. It uses event-stream.pipeline
    to assemble streams to:
      1 read from the input file
      2 split the input stream on newlines using `es.split`
      3 process each line and add data (length and toUpperCase)
      4 convert each piece of data to JSON
      5 write the new JSON to the specified output file

---
+++ example-streams.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const es = require('event-stream');
+const through2 = require('through2');
+
+const inputFile = process.argv[2];
+const outputFile = process.argv[3];
+
+if(!inputFile || !outputFile) {
+  console.log("Proper Usage: node example-stream.js <input> <output>");
+  process.exit();
+}
+
+es.pipeline(
+  fs.createReadStream(inputFile),
+  es.split(),
+  through2.obj(function(chunk, enc, callback) {
+    if(chunk.length > 0) {
+      this.push({length: chunk.length});
+      this.push({uppercase: chunk.toUpperCase()});
+    }
+    callback();
+  }),
+  es.map(function(data, callback) {
+    callback(null, JSON.stringify(data) + "\n");
+  }),
+  fs.createWriteStream(outputFile)
+);
